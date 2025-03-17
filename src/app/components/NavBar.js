162cfed80877f26f341f51510301299977bc1a90
Michael Zargari <michaelza8@gmail.com>
2025-03-17 22:56:03 +0200

    init - first component()

---
+++ src/app/components/NavBar.js
@@ -0,0 +1,9 @@
+export default function NavBar() {
+    return (
+        <div>
+            <h1 style={{color:"rgb(233, 29, 29)"}}>
+                My Protfolio
+            </h1>
+        </div>
+    );
+}
