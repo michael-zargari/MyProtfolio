90d256082b067e117b889dbb68a80930f7d12887
Michael Zargari <michaelza8@gmail.com>
2025-03-24 23:25:06 +0200

    added new router for projecs page

---
+++ src/app/Projects/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProjectsPage = () => {
+  return (
+    <>
+    <div>ProjectsPage</div>
+    <a href='/'>go to home page</a>
+    </>
+  )
+}
+
+export default ProjectsPage
